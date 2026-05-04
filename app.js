ed93e8397a968db6f0b91f7f2b5209fa2d4c6f0e
Lime-N-ade <aolszewski1@my.bcit.ca>
2026-05-03 17:11:22 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,220 @@
+require('./utils.js');
+require('dotenv').config(); 
+
+
+
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo').default;
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+
+const app = express();
+
+const Joi = require("joi");
+const mongoSanitizer = require('mongo-sanitizer').default;
+//import mongoSanitizer from 'mongo-sanitizer';
+
+const PORT = process.env.PORT || 3000;
+const expireTime = 24 * 60 * 60 * 1000; //expires after 1 day  (hours * minutes * seconds * millis)
+
+/* secret information section */
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_user_database = process.env.MONGODB_USER_DATABASE;
+const mongodb_session_database = process.env.MONGODB_SESSION_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+/* END secret section */
+
+const {database} = include('databaseConnection');
+const userCollection = database.db(mongodb_user_database).collection('users');
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.use(mongoSanitizer(
+    { replaceWith: '_'}
+));
+
+var mongoStore = MongoStore.create({
+    mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_session_database}`,
+    crypto: {
+        secret: mongodb_session_secret
+    }
+});
+
+app.use(session({ 
+    secret: node_session_secret,
+    store: mongoStore, //default is memory store 
+    saveUninitialized: false, 
+    resave: true
+}
+));
+
+// Routes
+app.get('/', (req, res) => {
+    if (req.session.authenticated) {
+        res.redirect('/loggedin');
+    } else {
+        res.send(`
+        <button onclick="window.location.href='/Login'">Login</button>
+        <button onclick="window.location.href='/signup'">SignUp</button><br>`);
+    }
+});
+
+app.get('/about', (req,res) => {
+    var color = req.query.color;
+
+    res.send("<h1 style='color:"+color+";'>Patrick Guichon</h1>");
+});
+
+app.get('/cat/:id', (req,res) => {
+
+    var cat = req.params.id;
+
+    if (cat == 1) {
+        res.send("Fluffy: <img src='/fluffy.gif' style='width:250px;'>");
+    }
+    else if (cat == 2) {
+        res.send("Socks: <img src='/socks.gif' style='width:250px;'>");
+    }
+    else {
+        res.send("Invalid cat id: "+cat);
+    }
+});
+
+app.post('/submitEmail', (req,res) => {
+    var email = req.body.email;
+    if (!email) {
+        res.redirect('/contact?missing=1');
+    }
+    else {
+        res.send("Thanks for subscribing with your email: "+email);
+    }
+});
+
+app.get('/signup', (req,res) => {
+    var html = `
+    create user
+    <form action='/submitUser' method='post'>
+    <input name='username' type='text' placeholder='username'>
+    <input name='password' type='password' placeholder='password'>
+    <button>Submit</button>
+    </form>
+    `;
+    res.send(html);
+});
+
+app.post('/submitUser', async (req,res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    const schema = Joi.object(
+        {
+            username: Joi.string().alphanum().max(20).required(),
+            password: Joi.string().max(20).required()
+        });
+    
+    const validationResult = schema.validate({username, password});
+    if (validationResult.error != null) {
+        console.log(validationResult.error);
+        res.redirect("/createUser");
+        return;
+    }
+
+    var hashedPassword = await bcrypt.hash(password, saltRounds);
+    
+    await userCollection.insertOne({username: username, password: hashedPassword});
+    console.log("Inserted user");
+
+    var html = "successfully created user";
+    res.send(html);
+});
+
+app.get('/login', (req,res) => {
+    var html = `
+    log in
+    <form action='/loggingin' method='post'>
+    <input name='username' type='text' placeholder='username'>
+    <input name='password' type='password' placeholder='password'>
+    <button>Submit</button>
+    </form>
+    `;
+    res.send(html);
+});
+
+app.post('/loggingin', async (req,res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    const schema = Joi.string().max(20).required();
+    const validationResult = schema.validate(username);
+    if (validationResult.error != null) {
+        console.log(validationResult.error);
+        res.redirect("/login");
+        return;
+    }
+
+    const result = await userCollection.find({username: username}).project({username: 1, password: 1, _id: 1}).toArray();
+
+    console.log(result);
+    if (result.length != 1) {
+        console.log("user not found");
+        res.redirect("/login");
+        return;
+    }
+    if (await bcrypt.compare(password, result[0].password)) {
+        console.log("correct password");
+        req.session.authenticated = true;
+        req.session.username = username;
+        req.session.cookie.maxAge = expireTime;
+
+        res.redirect('/loggedIn');
+        return;
+    }
+    else {
+        console.log("incorrect password");
+        res.redirect("/login");
+        return;
+    }
+});
+
+app.get('/loggedIn', (req, res) => {
+    if (!req.session.authenticated) {
+        return res.redirect('/');
+    }
+
+    const images = ['image1.jpg', 'image2.jpg', 'image3.jpg'];
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+
+    res.send(`
+        <h1>Hello, ${req.session.username}!</h1>
+        <img src='/${randomImage}' style='width:250px;'>
+        <br>
+        <button onclick="window.location.href='/logout'">Sign out</button>
+    `);
+});
+
+app.get('/logout', (req,res) => {
+	req.session.destroy();
+    var html = `
+    You are logged out.
+    `;
+    res.send(html);
+});
+
+app.use((req,res) => {
+    res.status(404);
+    res.send("Page not found - 404");
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
